99f216283021f9c2c3323e0154a871bef22096fc
sisackov <1123889+sisackov@users.noreply.github.com>
2021-12-02 13:31:35 +0200

    js start

---
+++ js/game.js
@@ -0,0 +1,77 @@
+/** 2d array representation of the game board */
+const initialMatrix = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0],
+    [0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 0, 0],
+    [0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 0, 0],
+    [0, 0, 3, 3, 3, 3, 3, 0, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 0, 4],
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+    [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+    [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+    [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+    [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+    [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+];
+
+export const Resource = {
+    SKY: 0,
+    CLOUD: 1,
+    TREE: 2,
+    TRUNK: 3,
+    STONE: 4,
+    GRASS: 5,
+    DIRT: 6,
+};
+export const resourceMap = new Map();
+
+function initGame() {
+    for (const [key, value] of Object.entries(Resource)) {
+        let resourceObj = {
+            name: key,
+            value: value,
+        };
+
+        let list;
+        switch (value) {
+            case Resource.SKY:
+                list = ['sky'];
+                break;
+            case Resource.CLOUD:
+                list = ['cloud'];
+                break;
+            case Resource.TREE:
+                list = ['tile__image', 'tree'];
+                break;
+            case Resource.TRUNK:
+                list = ['tile__image', 'trunk'];
+                break;
+            case Resource.STONE:
+                list = ['tile__image', 'stone'];
+                break;
+            case Resource.GRASS:
+                list = ['tile__image', 'grass'];
+                break;
+            case Resource.DIRT:
+                list = ['tile__image', 'dirt'];
+                break;
+            default:
+                list = [];
+                break;
+        }
+        resourceObj['classList'] = list;
+        resourceMap.set(value, resourceObj);
+    }
+}
+
+initGame();
+
+console.log(resourceMap);
---
+++ js/main.js
@@ -0,0 +1,6 @@
+import { getInitialMatrixCopy } from './game.js';
+
+let matrix = getInitialMatrixCopy();
+
+console.log(...matrix);
+console.log(...getInitialMatrixCopy());
